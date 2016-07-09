103c6a040b41a82789b702a2f505a577ac3f05aa
Kevin Grandon <kevingrandon@yahoo.com>
2016-07-09 16:05:51 -0700

    Add blink script.

--- python_test_scripts/button.py
+++ python_test_scripts/button.py
@@ -3,10 +3,24 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
+# Button
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+# LED
+GPIO.setup(18, GPIO.OUT)
+
+## Blinks the LED
+def Blink(numTimes,speed):
+	for i in range(0,numTimes):
+		GPIO.output(18,True) ## Switch on pin 18
+		time.sleep(speed) ## Wait
+		GPIO.output(18,False) ## Switch off pin 18
+		time.sleep(speed)
+		GPIO.cleanup()
+
+
 while True:
-    input_state = GPIO.input(17)
-    if input_state == False:
-        print('Button Pressed')
-        time.sleep(0.2)
+	input_state = GPIO.input(17)
+	if input_state == False:
+		Blink(10, 100)
+		time.sleep(0.2)
