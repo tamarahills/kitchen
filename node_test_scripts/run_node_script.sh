079d8c259a9d4cd8c1862ba0e41b11a586cb93dc
Kevin Grandon <kevingrandon@yahoo.com>
2016-07-12 20:31:31 -0700

    Adjust timeout.

--- node_test_scripts/run_node_script.sh
+++ node_test_scripts/run_node_script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # This script is called from /crontab and runs the node server on startup.
 # We have to sleep for a while on startup to avoid strange issues with GPIO pins.
-sleep 20
+sleep 15
 sudo node index.js > tmp.log
