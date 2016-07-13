1c05486b3d62a0bc0eb7f47d36efd034748579c8
Kevin Grandon <kevingrandon@yahoo.com>
2016-07-12 23:46:24 -0700

    include full path of folder.

--- node_test_scripts/run_node_script.sh
+++ node_test_scripts/run_node_script.sh
@@ -2,4 +2,4 @@
 # This script is called from /crontab and runs the node server on startup.
 # We have to sleep for a while on startup to avoid strange issues with GPIO pins.
 sleep 15
-sudo node index.js > tmp.log
+sudo node /home/pi/workspace/kitchen/node_test_scripts/index.js > tmp.log
