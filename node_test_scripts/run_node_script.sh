07532b1315111acf2ba353cb1cf1374736be4c93
Kevin Grandon <kevingrandon@yahoo.com>
2016-07-12 20:21:27 -0700

    Remove run_node_script.
    Turns out it's better to run the camera node script as a crontab instead of rc.local for some reason.

--- node_test_scripts/run_node_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# This script is called from /etc/rc.local and runs the node server on startup.
-# We have to sleep for a while on startup to avoid strange issues with GPIO pins.
-sleep 10
-sudo node index.js
