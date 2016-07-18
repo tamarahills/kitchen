9dfb74438c17b9489f4706f0b582e67e06a34748
Tamara Hills <thills@mozilla.com>
2016-07-18 12:06:53 -0400

    Issue #16 - Integrate the device script with the node server.

---
+++ device/config.json.tpl
@@ -0,0 +1,3 @@
+{
+  "user_key": "<Userid key>"
+}
--- device/index.js
+++ device/index.js
@@ -1,6 +1,8 @@
 var NodeWebcam = require('node-webcam')
 var five = require('johnny-five');
 var Raspi = require('raspi-io');
+var watson = require('watson-developer-cloud');
+var fs = require('fs');
 
 var BUTTON_DOUBLE_CLICK_TIMEOUT = 300
 
@@ -45,6 +47,8 @@ board.on("ready", function() {
 					led.stop();
 					led.off();
 
+          processPicture();
+
 					clearTimeout(processTimeout);
 					processTimeout = null;
 
@@ -70,4 +74,84 @@ function takePicture (cb) {
 
 	var webcam = NodeWebcam.create(opts);
 	webcam.capture(__dirname + '/test_picture', cb);
+}
+
+function processPicture() {
+  var item_array = [];
+
+  var visual_recognition = watson.visual_recognition({
+    api_key: '30a8e58eb3163f8319ebea84ecd1b1b0027b6038',
+    version: 'v3',
+    version_date: '2016-05-19'
+  });
+
+  var params = {
+    images_file: fs.createReadStream('./fruitbowl.jpg'),
+    classifier_ids: ['greenpepper_194357852', 'orange_2010321797', 'banana_760231312', 'default']
+  };
+
+  visual_recognition.classify(params, function(err, res) {
+    if (err)
+      console.log(err);
+    else {
+      console.log(JSON.stringify(res, null, 2));
+      console.log('\n\n');
+      // There should only be one imaged processed at a time.
+      if (res.images_processed == 1) {
+        var classifiers = res.images[0].classifiers;
+        for (var i = 0; i < classifiers.length; i++) {
+          var classifier = classifiers[i];
+          // If it's the default watson classifier, then we can just use the "class" attribute.
+          if (classifier.classifier_id.localeCompare('default') == 0) {
+            console.log('classes length:' + classifier.classes.length);
+            for (var j = 0; j < classifier.classes.length; j++) {
+              console.log(classifier.classes[j].class);
+              item_array.push(classifier.classes[j].class)
+            }
+          } else {
+            // If the classifier is one of our custom ones, then we can just use the
+            // "name" attribute
+            item_array.push(classifier.name);
+          }
+        }
+      } else {
+        console.log('Error processing the image file:');
+      }
+    }
+    // Post the Results to the server.
+    item_array.forEach(function (item, index, array) {
+      console.log(item);
+    });
+    if (item_array.length > 0) {
+      postToServer(item_array[0]);
+    } else {
+      postToServer('NoResults');
+    }
+
+  });
+
+  function postToServer(itemString) {
+    var post_options = {
+//      host: 'controller-dev.us-east-1.elasticbeanstalk.com',
+      host: '127.0.0.1',
+      port: '8080',
+      path: '/item',
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      }
+    };
+
+    var post_req = http.request(post_options, function(res) {
+      console.log('completed the post: ' + res.statusCode);
+    });
+    // post the data
+    var post_data = {
+      //TODO: read this from file as an id.
+      userid: 'tamarajhills',
+      item: itemString
+    };
+    post_req.write(event_string);
+    post_req.end();
+  }
 }
