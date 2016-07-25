a88f6dd9190670ef0fabcc3be7d22b25ec1afefa
Russ Nicoletti <rnicoletti@mozilla.com>
2016-07-25 22:38:19 +0000

    Update entire 'device' directory at midnight

--- device/classifiers_update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# This script is called from /crontab and runs at midnight every night.
-# It updates the classifers.json so we can add more classifiers to the project
-# without updation of the physical device or asking user to do something.
-wget https://raw.githubusercontent.com/tamarahills/kitchen/master/device/classifiers.json
---
+++ device/update.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This script is called as a cron job; it runs at midnight every night.
+# It updates the ´device' directory so we can add more classifiers to the project
+# and update the device script without needing the user to take any action.
+cd /home/pi/workspace/kitchen/device
+git pull origin master
+echo "rebooting in 5 seconds..."
+sleep 5
+reboot
