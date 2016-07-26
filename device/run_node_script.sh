e1265fab25eb0aaf36e7dc3e73c8db4d3776d097
Russ Nicoletti <rnicoletti@mozilla.com>
2016-07-26 17:05:26 +0000

    Log to workspace directory

--- device/run_node_script.sh
+++ device/run_node_script.sh
@@ -2,4 +2,4 @@
 # This script is called from /crontab and runs the node server on startup.
 # We have to sleep for a while on startup to avoid strange issues with GPIO pins.
 sleep 15
-sudo node /home/pi/workspace/kitchen/device/index.js > tmp.log
+sudo node /home/pi/workspace/kitchen/device/index.js > /home/pi/workspace/kitchen/device/tmp.log
