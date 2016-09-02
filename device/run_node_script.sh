679b6c46058e5cb73656b6d6b7574e52d03c3fd1
Russ Nicoletti <rnicoletti@mozilla.com>
2016-09-02 15:33:48 -0700

    Fixed issue setting PATH

--- device/run_node_script.sh
+++ device/run_node_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This script is called from /crontab and runs the node server on startup.
 # We have to sleep for a while on startup to avoid strange issues with GPIO pins.
-PATH=dirname $(which node)
+PATH=`dirname $(which node)`
 sleep 15
 sudo node /home/pi/workspace/kitchen/device/index.js > /home/pi/workspace/kitchen/device/tmp.log
